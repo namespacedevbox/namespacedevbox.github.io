2806b0eec7b187f3d7cbf9297e8fbb1f1570da36
namespacedevbox <34470377+namespacedevbox@users.noreply.github.com>
2019-11-22 22:38:23 +0200

    Add files via upload

---
+++ main/js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+        animationTime = 300,
+        framesCount = 20;
+
+    anchors.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+            let scroller = setInterval(function () {
+                let scrollBy = coordY / framesCount;
+
+                if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                    window.scrollBy(0, scrollBy);
+                } else {
+                    window.scrollTo(0, coordY);
+                    clearInterval(scroller);
+                }
+            }, animationTime / framesCount);
+        });
+    });
+});
